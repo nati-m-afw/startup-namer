6a88f826413aca84e2fb805a5331bcc8fdb4e022
nati-m <it.natnael.mekonnen@gmail.com>
2020-02-27 18:18:25 +0300

    Billy

---
+++ lib/bullseye.dart
@@ -0,0 +1,14 @@
+//This is a demo of dart
+//About billy the bully and his sucky personality
+class Bully{
+  String name;
+  String personality;
+  Bully(this.name, this.personality);
+  String get bully => "$name has a $personality personality";
+}
+void main(){
+  var billy = new Bully("Billy", "sucky");
+  print(billy.bully);
+  print(billy.name);
+
+}
