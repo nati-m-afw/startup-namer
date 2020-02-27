33c95cbf614008bfc6b466871710dd47e399efb6
nati-m <it.natnael.mekonnen@gmail.com>
2020-02-27 18:39:37 +0300

    |--Named Parameters--| Functions can have named parameters or positional (the typical style of parameter list), but you can’t mix the two styles.

--- lib/bullseye.dart
+++ lib/bullseye.dart
@@ -1,14 +1,30 @@
-//This is a demo of dart
-//About billy the bully and his sucky personality
-class Bully{
-  String name;
-  String personality;
-  Bully(this.name, this.personality);
-  String get bully => "$name has a $personality personality";
-}
-void main(){
-  var billy = new Bully("Billy", "sucky");
-  print(billy.bully);
-  print(billy.name);
+////This is a demo of dart
+////About billy the bully and his sucky personality
+//class Bully{
+//  String name;
+//  String personality;
+//  Bully(this.name, this.personality);
+//  String get bully => "$name has a $personality personality";
+//}
+//void main(){
+//  var billy = new Bully("Billy", "sucky");
+//  print(billy.bully);
+//  print(billy.name);
+//
+//}
 
+greet(String name) {
+  print("Hello, $name");
+}
+
+class MyClass {
+  greetAgain({ Function f, String n = "human" }) {
+    f(n);
+  }
+}
+main() {
+  MyClass mc = new MyClass();
+  greet("Frank");
+  mc.greetAgain( f : greet, n : "Traci" );
+  mc.greetAgain( f : greet);
 }
