f94331ef752a0dd7f48a11cb2f896453f84e77aa
nati-m <it.natnael.mekonnen@gmail.com>
2021-06-24 10:31:07 +0300

    Stateful Widegets

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  debugPaintSizeEnabled=true;
+  // WidgetsApp.showPerformanceOverlayOverride=true;
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget{
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return MaterialApp(
-      title: 'Welcome my peeps',
+  Widget build(BuildContext context) => MaterialApp(
+      title: 'FlutterApp',
+      theme: ThemeData(
+        primaryColor: Colors.deepOrangeAccent,
+        textTheme: TextTheme(
+          bodyText2: TextStyle(
+              fontSize: 20,
+              color: Colors.white70),
+        )
+      ),
       home: Scaffold(
+        backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: Text('Welcome to flutter my peeps'),
+          title: Text('Still runnin....'),
         ),
-        body: Center(
-          child: Text('this is awsome'),
+        body: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.fromLTRB(0,30,0,0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  HomePage(),
+                  HomePage(),
+              ]
+              ),
+              Row(
+                  children: [
+                    HomePage(),
+                    HomePage(),
+                  ]
+              ),
+            ],
+          ),
         ),
       ),
     );
+}
+
+class HomePage extends StatefulWidget{
+
+  @override
+  State<StatefulWidget> createState() {
+    return _CounterState();
   }
+
+}
+
+class _CounterState extends State<HomePage>{
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      this._counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Text('$_counter'),
+          ElevatedButton(onPressed: _increment, child: Icon(Icons.add))
+        ],
+      ),
+    );
+  }
+  
 }
